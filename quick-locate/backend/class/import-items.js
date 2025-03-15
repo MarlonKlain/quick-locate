4eb6b09506a15192223e400f19c4e15d93b58f5b
“[Marlon <marlondossantosklain@gmail.com>
2025-03-15 15:24:12 -0300

    .

--- quick-locate/backend/class/import-items.js
+++ quick-locate/backend/class/import-items.js
@@ -1,11 +1,11 @@
-import * as XLSX from "xlsx/xlsx.mjs";
 import { readFileSync } from "fs";
+import * as XLSX from "xlsx/xlsx.mjs";
 
 export class ImportTtems {
     async storeData(){
         const buf = readFileSync("items.xlsx");
         /* buf is a Buffer */
-        const workbook = read(buf);
+        const workbook = XLSX.read(buf);
         // Select the first sheet
         const sheetName = workbook.SheetNames[0]; 
         // Get the first sheet name
